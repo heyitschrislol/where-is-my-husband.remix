9210f9c8d00c5c1c77e6e394431d014039d5dfdb
Christopher Hendl <chendl@gmail.com>
2026-08-03 00:14:42 -0500

    shrunk everything; more sprite changes

---
+++ scripts/broom_blinder.gd
@@ -0,0 +1,22 @@
+extends Polygon2D
+
+@export var room_name: String = ""  # must match a "<ROOM>_REVEALED" flag in Gamedata
+
+const BLINDER_COLOR := Color(0.06, 0.06, 0.08, 0.96)  # near-black, slightly blue-tinted, near-opaque
+
+func _ready() -> void:
+	color = BLINDER_COLOR
+	z_as_relative = false
+	z_index = 50  # make sure it draws above tiles/furniture regardless of tree order
+
+	if Gamedata.get(room_name + "_REVEALED"):
+		visible = false
+		return
+
+	Gamedata.room_revealed.connect(_on_room_revealed)
+
+func _on_room_revealed(revealed_room: String) -> void:
+	if revealed_room != room_name:
+		return
+	visible = false
+	# later: swap this for a fade-out tween once the scene-fade system is in place
